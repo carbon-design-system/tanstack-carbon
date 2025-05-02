2cfbbe78f83381b86207e43f9e89adf0043b94d2
devadula-nandan <devadula.nandan@gmail.com>
2025-05-02 18:43:53 +0530

    fix: add controlled expansion similar to react

--- web-components/row-expansion/src/row-expansion.ts
+++ web-components/row-expansion/src/row-expansion.ts
@@ -1,11 +1,12 @@
 import { LitElement, css, unsafeCSS, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import { repeat } from 'lit/directives/repeat.js';
 import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
   TableController,
+  getExpandedRowModel,
 } from '@tanstack/lit-table';
 import '@carbon/web-components/es/components/data-table/index.js';
 import { makeData } from './makeData';
@@ -14,6 +15,7 @@ import indexStyles from './index.scss?inline';
 const styles = css`
   ${unsafeCSS(indexStyles)}
 `;
+
 type Resource = {
   id: string;
   name: string;
@@ -57,15 +59,58 @@ const data: Resource[] = makeData(7);
 export class MyBasicTable extends LitElement {
   private tableController = new TableController<Resource>(this);
 
+  @state()
+  private expanded: Record<string, boolean> = {};
+
+  @state()
+  private isAllExpanded: boolean = false;
+
   render() {
     const table = this.tableController.table({
       columns,
       data,
+      state: {
+        expanded: this.expanded,
+      },
       getCoreRowModel: getCoreRowModel(),
+      getExpandedRowModel: getExpandedRowModel(),
+      onExpandedChange: (updater) => {
+        this.expanded =
+          typeof updater === 'function'
+            ? (updater(this.expanded) as Record<string, boolean>)
+            : (updater as Record<string, boolean>);
+      },
     });
+
+    const handleRowExpansion = (e: CustomEvent): void => {
+      // prevent default cds-table expansion
+      e.preventDefault();
+      const row = e.target as HTMLElement;
+      const rowId = row.dataset.rowId;
+      // console.log(row, rowId);
+      if (rowId) {
+        this.expanded = {
+          ...this.expanded,
+          [rowId]: !this.expanded[rowId],
+        };
+        this.requestUpdate();
+      } else {
+        // the event is not from a row but from header's expand all button
+        if (!this.isAllExpanded) {
+          table.getRowModel().rows.forEach((row) => {
+            this.expanded[row.id] = true;
+          });
+          this.isAllExpanded = true;
+        } else {
+          this.expanded = {};
+          this.isAllExpanded = false;
+        }
+      }
+    };
 
     return html`
       <cds-table
+        @cds-table-row-expando-beingtoggled=${handleRowExpansion}
         style="width: ${table.getCenterTotalSize()}px"
         expandable
         batch-expansion>
@@ -99,7 +144,10 @@ export class MyBasicTable extends LitElement {
             table.getRowModel().rows,
             (row) => row.id,
             (row) => html`
-              <cds-table-row>
+              <cds-table-row
+                data-row-id="${row.id}"
+                ?expanded="${this.expanded[row.id]}"
+                data-expanded="${this.expanded[row.id]}">
                 ${repeat(
                   row.getVisibleCells(),
                   (cell) => cell.id,
