87bb9be0a14a8a3410da166be57614e8b0c8b18e
Sinta Augustine <sintaaugustine@gmail.com>
2025-05-27 16:40:56 +0530

    feat: selectable nested rows

--- react/selectable-nested-rows/src/Example/SelectableNestedRows.tsx
+++ react/selectable-nested-rows/src/Example/SelectableNestedRows.tsx
@@ -5,91 +5,134 @@ import {
   useReactTable,
   getCoreRowModel,
   getExpandedRowModel,
-  ColumnDef,
   flexRender,
+  createColumnHelper,
+  // FilterFn,
+  // PaginationState,
+  // getPaginationRowModel,
+  // getFilteredRowModel,
 } from '@tanstack/react-table';
-import { DataTable, TableContainer, Button } from '@carbon/react';
+import { DataTable, Button, Checkbox } from '@carbon/react';
 import { ChevronRight } from '@carbon/react/icons';
-const { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } =
-  DataTable;
+const {
+  Table,
+  TableContainer,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} = DataTable;
 
 import { makeData, Resource } from './makeData';
 
 export const SelectableNestedRows = () => {
-  const columns = React.useMemo<ColumnDef<Resource>[]>(
-    () => [
-      {
-        accessorKey: 'name',
-        header: ({ table }) => (
-          <div className="flex">
-            <Button
-              {...{
-                onClick: table.getToggleAllRowsExpandedHandler(),
-              }}
-              className="row-expander"
-              kind="ghost"
-              size="sm">
-              {table.getIsAllRowsExpanded() ? (
-                <ChevronRight className="row-expanded-icon" />
-              ) : (
-                <ChevronRight />
-              )}
-            </Button>
-            Name
-          </div>
-        ),
-        cell: ({ row, getValue }) => (
-          <div
-            style={{
-              // Since rows are flattened by default,
-              // we can use the row.depth property
-              // and paddingLeft to visually indicate the depth
-              // of the row
-              paddingLeft: `${
-                row.depth * 2 + (!row.getCanExpand() ? 2 : 0)
-              }rem`,
-            }}>
-            <div className="flex">
-              {row.getCanExpand() ? (
-                <Button
-                  {...{
-                    onClick: row.getToggleExpandedHandler(),
-                    style: { cursor: 'pointer' },
-                  }}
-                  className="row-expander"
-                  kind="ghost"
-                  size="sm">
-                  {row.getIsExpanded() ? (
-                    <ChevronRight className="row-expanded-icon" />
-                  ) : (
-                    <ChevronRight />
-                  )}
-                </Button>
-              ) : null}{' '}
-              {getValue<boolean>()}
-            </div>
-          </div>
-        ),
-      },
-      {
-        accessorFn: (row) => row.rule,
-        id: 'rule',
-        cell: (info) => info.getValue(),
-        header: () => <span>Rule</span>,
-      },
-      {
-        accessorKey: 'status',
-        header: () => 'Status',
-      },
-      {
-        accessorKey: 'other',
-        header: () => <span>Other</span>,
-      },
-    ],
-    []
-  );
+  const columnHelper = createColumnHelper<Resource>();
 
-  const [data] = React.useState(() => makeData(10, 5, 3));
+  const columns = [
+    columnHelper.accessor((row) => row.name, {
+      id: 'name',
+      header: ({ table }) => (
+        <div className="flex">
+          <Button
+            {...{
+              onClick: table.getToggleAllRowsExpandedHandler(),
+            }}
+            className="row-expander"
+            kind="ghost"
+            size="sm">
+            {table.getIsAllRowsExpanded() ? (
+              <ChevronRight className="row-expanded-icon" />
+            ) : (
+              <ChevronRight />
+            )}
+          </Button>
+          {/* TableSelectAll throws DOM nesting error, using Checkbox instead to avoid this */}
+          <Checkbox
+            {...{
+              className: 'row-selector',
+              checked: table.getIsAllPageRowsSelected(),
+              indeterminate: table.getIsSomePageRowsSelected(),
+              onChange: () => {
+                const isIndeterminate = table.getIsSomeRowsSelected();
+                if (!isIndeterminate) {
+                  table.toggleAllPageRowsSelected(true);
+                }
+                if (table.getIsAllPageRowsSelected()) {
+                  table.toggleAllRowsSelected(false);
+                  return;
+                }
+                if (isIndeterminate) {
+                  table.toggleAllPageRowsSelected(true);
+                  return;
+                }
+              },
+              id: 'batch-checkbox',
+              labelText: 'header checkbox',
+              hideLabel: true,
+            }}
+          />
+          <span className="row-content">Name</span>
+        </div>
+      ),
+      cell: ({ row, getValue }) => (
+        <div
+          style={{
+            // Since rows are flattened by default,
+            // we can use the row.depth property
+            // and paddingLeft to visually indicate the depth
+            // of the row
+            paddingLeft: `${row.depth * 2 + (row.getCanExpand() ? 0 : 1)}rem`,
+          }}>
+          <div className="flex">
+            {row.getCanExpand() ? (
+              <Button
+                {...{
+                  onClick: row.getToggleExpandedHandler(),
+                  style: { cursor: 'pointer' },
+                }}
+                className="row-expander"
+                kind="ghost"
+                size="sm">
+                {row.getIsExpanded() ? (
+                  <ChevronRight className="row-expanded-icon" />
+                ) : (
+                  <ChevronRight />
+                )}
+              </Button>
+            ) : null}
+            <Checkbox
+              {...{
+                className: 'row-selector',
+                checked: row.getIsSelected(),
+                disabled: !row.getCanSelect(),
+                indeterminate: row.getIsSomeSelected(),
+                onChange: row.getToggleSelectedHandler(),
+                id: `batch-checkbox__${row.id}`,
+                labelText: 'row checkbox',
+                hideLabel: true,
+              }}
+            />
+            <span className="row-content">{getValue<boolean>()}</span>
+          </div>
+        </div>
+      ),
+    }),
+    columnHelper.accessor('rule', {
+      header: 'Rule',
+    }),
+    columnHelper.accessor('status', {
+      header: 'Status',
+    }),
+    columnHelper.accessor('other', {
+      header: 'Other',
+    }),
+    columnHelper.accessor('example', {
+      header: 'Example',
+    }),
+  ];
+
+  const [data] = React.useState(() => makeData(10, 5, 3, 2));
 
   const [expanded, setExpanded] = React.useState<ExpandedState>({});
 
