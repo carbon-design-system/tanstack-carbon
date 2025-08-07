c71a38f881d9f22c8c55eba1f027defe9fe39332
Sinta Augustine <sintaaugustine@gmail.com>
2025-08-07 17:10:35 +0530

    feat: expansion indicators for nested rows web components

--- web-components/nested-rows/src/nestedRowTable.ts
+++ web-components/nested-rows/src/nestedRowTable.ts
@@ -44,30 +44,41 @@ export class NestedRowTable extends LitElement {
               : 'row-expandable-icon',
           })}
         </cds-button>
-        <span>Name</span>
+        <span class="row-content">Name</span>
       </div>`,
       cell: ({ row, renderValue }) => {
         return html` <div
+          class="flex"
           style="${styleMap({
             paddingLeft: `${row.depth * 2 + (!row.getCanExpand() ? 2 : 0)}rem`,
           })}">
-          <div class="flex">
-            ${row.getCanExpand()
-              ? html`<cds-button
-                  @click=${row.getToggleExpandedHandler()}
-                  class="row-expander"
-                  kind="ghost"
-                  size="sm">
-                  ${ChevronRight({
-                    slot: 'icon',
-                    class: row.getIsExpanded()
-                      ? `row-expanded-icon`
-                      : 'row-expandable-icon',
-                  })}
-                </cds-button>`
-              : null}
-            ${renderValue()}
-          </div>
+          ${row.getCanExpand()
+            ? html`<cds-button
+                @click=${row.getToggleExpandedHandler()}
+                class="row-expander"
+                kind="ghost"
+                size="sm">
+                ${ChevronRight({
+                  slot: 'icon',
+                  class: row.getIsExpanded()
+                    ? `row-expanded-icon`
+                    : 'row-expandable-icon',
+                })}
+              </cds-button>`
+            : null}
+          <span class="row-content">${renderValue()}</span>
+          <div
+            class="border-line"
+            style="${styleMap({
+              width: `${
+                row.depth * 2 + (row.depth || row.getIsExpanded() ? 3 : 0)
+              }rem`,
+            })}"></div>
+          <div
+            class="expansion-indicator"
+            style="${styleMap({
+              left: `${this._expIndPos}rem`,
+            })}"></div>
         </div>`;
       },
     }),
@@ -88,6 +99,41 @@ export class NestedRowTable extends LitElement {
 
   @state()
   private _expanded = {};
+
+  // expansion indicator: state variables
+  @state()
+  private _hoveredRowIds: string[] = [];
+
+  @state()
+  private _expIndPos = 0;
+
+  // expansion indicator: method to get row ids of all sub-rows
+  private _getAllSubRowIds(row: Row<Resource>) {
+    const ids: string[] = [];
+
+    const collectIds = (row: Row<Resource>) => {
+      if (row.subRows && row.subRows.length > 0) {
+        for (const subRow of row.subRows) {
+          ids.push(subRow.id);
+          collectIds(subRow); // Recursive for deep nesting
+        }
+      }
+    };
+
+    collectIds(row);
+
+    return ids;
+  }
+
+  // for expansion indicator
+  private _onRowHover(row: Row<Resource>) {
+    if (row.getCanExpand() && row.getIsExpanded()) {
+      this._expIndPos = row.depth * 2 + 1;
+      this._hoveredRowIds = this._getAllSubRowIds(row);
+    } else {
+      this._hoveredRowIds = [];
+    }
+  }
 
   render() {
     const table = this.tableController.table({
@@ -136,7 +182,12 @@ export class NestedRowTable extends LitElement {
             table.getRowModel().rows,
             (row) => row.id,
             (row) => html`
-              <cds-table-row>
+              <cds-table-row
+                @mouseenter="${() => this._onRowHover(row)}"
+                @mouseleave="${() => (this._hoveredRowIds = [])}"
+                class="${this._hoveredRowIds.includes(row.id)
+                  ? 'row-hovered'
+                  : ''}">
                 ${repeat(
                   row.getVisibleCells(),
                   (cell) => cell.id,
@@ -165,13 +216,22 @@ export class NestedRowTable extends LitElement {
       place-items: center;
     }
 
+    :host cds-table-cell:first-of-type,
+    :host cds-table-header-cell:first-of-type {
+      padding-inline-start: 0.5rem;
+    }
+
     .flex {
       display: flex;
       align-items: center;
+      position: relative;
     }
 
     .row-expander {
-      margin-right: 1rem;
+      padding: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
 
     .row-expandable-icon {
@@ -183,6 +243,32 @@ export class NestedRowTable extends LitElement {
       color: var(--cds-icon-primary, #161616);
       transform: rotate(0.25turn);
       transition: transform 150ms ease-in; // replace with carbon motion easing
+    }
+
+    .row-content {
+      flex: 1 0 auto;
+      padding-left: 1rem;
+      height: 3rem;
+      align-content: center;
+    }
+
+    .border-line {
+      content: '';
+      position: absolute;
+      bottom: -1px;
+      left: -0.5rem;
+      height: 1px;
+      background-color: var(--cds-layer, #f4f4f4);
+    }
+
+    .row-hovered .expansion-indicator {
+      content: '';
+      position: absolute;
+      top: 0;
+      width: 1px;
+      height: 3.125rem;
+      background-color: var(--cds-border-subtle-01, #c6c6c6);
+      z-index: 1;
     }
   `;
 }
